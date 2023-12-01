2f4921b5e5431a09b504804e9f178ddbeacf4c0f
VitayG <VitalyG-17@yandex.ru>
2023-12-01 16:14:22 +0300

    - добавлена функциональность лучшего результата - добавлена функция, запрещающая ввод числе не из диапазона - немного отрефакторен код

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 'use strict';
 
-// console.log(document.querySelector('.guess-message').textContent);
-// document.querySelector('.guess-message').textContent = 'Верно!';
-// document.querySelector('.question').textContent = 7;
-// document.querySelector('.score').textContent = 11;
-// document.querySelector('.number-input').value = 99
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 10;
-let bestScore;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const quessingNumber = Number(document.querySelector('.number-input').value);
@@ -24,53 +18,62 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.guess-message').style.color = '#00FF00';
     document.querySelector('.question').style.width = '50rem';
-  }
-  //Игрок выбрал маленькое число
-  else if (quessingNumber < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.guess-message').textContent =
-        'Загаданное число больше!';
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.guess-message').style.color = 'red';
-    } else {
-      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
-      document.querySelector('.guess-message').style.color = 'red';
-    }
-  }
-  //Игрок выбрал большое число
-  else if (quessingNumber > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.guess-message').textContent =
-        'Загаданное число меньше!';
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.guess-message').style.color = 'red';
-    } else {
-      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
   }
 
-  if(score>=7) {
-    document.querySelector('.score').style.color = '#7CFC00'
-  } else if (score <7 && score >=5 ){ 
-    document.querySelector('.score').style.color = '#EC7C26'
+  //Вывод подсказки в зависимости от ввода игрока
+  else if (quessingNumber !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent =
+        quessingNumber > secretNumber
+          ? 'Загаданное число меньше!'
+          : 'Загаданное число больше!';
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.guess-message').style.color = 'red';
+    } else {
+      document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+      document.querySelector('.guess-message').style.color = 'red';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  //Изменение цвета в зависимости от кол-ва очков
+  if (score >= 7) {
+    document.querySelector('.label-score').style.color = '#7CFC00';
+  } else if (score < 7 && score >= 5) {
+    document.querySelector('.label-score').style.color = '#F75E25';
   } else {
-    document.querySelector('.score').style.color = '#FF0033'
+    document.querySelector('.label-score').style.color = '#FF0033';
   }
 });
 
+//Логика после нажатия на кнопку "Сначала!"
 document.querySelector('.again').addEventListener('click', function () {
-  score = 10
+  score = 10;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   console.log(secretNumber);
 
   document.querySelector('.question').textContent = '???';
-  document.querySelector('body').style.backgroundColor = 'black';
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess-message').textContent = 'Начни угадывать';
   document.querySelector('.guess-message').style.color = 'white';
+  document.querySelector('body').style.backgroundColor = 'black';
   document.querySelector('.question').style.width = '25rem';
   document.querySelector('.number-input').value = null;
-
 });
+
+//Проверка введенного числа
+function checkInputValue(input) {
+  let value = parseInt(input.value, 10);
+
+  if (isNaN(value) || value < 1) {
+    input.value = 1;
+  } else if (value > 20) {
+    input.value = 20;
+  }
+}
